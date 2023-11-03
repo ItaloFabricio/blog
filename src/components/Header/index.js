e646b84a210152e196a39628143d13f621862e76
ítalo Fabrício <italofps65@gmail.com>
2023-11-03 01:05:13 -0300

    feat: estilizando o menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,27 @@
-
+import logosvg from '../../svg/logo.svg';
 
 const Header = () => {
 
-    let contador = 0;
+    const descLogo = 'Essa é a logo do blog';
 
     return (
-        <h1>Header.js{contador}</h1>
+        <>
+            <header className='flex-space-between'>
+                <div className="logo">
+                    <img src={logosvg} alt={descLogo}></img>
+                </div>
+                <div className="search">
+                    <input type='text' className='input-search' name='search'></input>
+                </div>
+                <div className="menu">
+                    <ul className='menu'>
+                        <li> <a href='#' className='nav-link'> Categories </a> </li>
+                        <li> <a href='#' className='nav-link'> About </a> </li>
+                        <li> <a href='#' className='nav-link'> Contact </a> </li>
+                    </ul>
+                </div>
+            </header>
+        </>
     );
 }
 
